ef15ef98b3e58b632f362e495a14dfafea6b5a8d
Jaswanth sai <119839943+jaswanthsaik@users.noreply.github.com>
2024-08-06 23:21:52 +0530

    add

--- mobile.js
+++ mobile.js
@@ -69,6 +69,11 @@ class Paper {
       this.holdingPaper = false;
       this.rotating = false;
     });
+    paper.addEventListener('mouseup', () => {
+      this.holdingPaper = false;
+    });
+
+    paper.addEventListener('click', showNextPaper);
 
     // For two-finger rotation on touch screens
     paper.addEventListener('gesturestart', (e) => {
@@ -87,3 +92,14 @@ papers.forEach(paper => {
   const p = new Paper();
   p.init(paper);
 });
+window.onload = () => {
+  papers.forEach((paperElement, index) => {
+    if (index === 0) {
+      paperElement.classList.add('visible');
+    } else {
+      paperElement.classList.remove('visible');
+    }
+    const paper = new Paper();
+    paper.init(paperElement);
+  });
+};
